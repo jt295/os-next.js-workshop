37f94af9d025600ef4c7664ce84027bf3d98b29d
James Trivett <James.Trivett@os.uk>
2024-06-19 11:35:48 +0100

    Task 1 completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 export default async function Home() {
-  const foo = await fetch(process.env.BASE_API_URL + "/").then((res) =>
-    res.text()
-  );
-
   return (
     <main className="p-8 container mx-auto">
       <h1>This is a test!</h1>
       <p className="bg-black text-white p-4 rounded drop-shadow my-4">
         This is some text that we have styled
       </p>
-      {foo}
     </main>
   );
 }
