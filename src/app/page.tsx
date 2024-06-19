6e64aed1ccc6c38c0d4710fa3cd3df4140ed22e6
James Trivett <James.Trivett@os.uk>
2024-06-19 11:37:17 +0100

    starting point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
 export default async function Home() {
-  return (
-    <main className="p-8 container mx-auto">
-      <h1>This is a test!</h1>
-      <p className="bg-black text-white p-4 rounded drop-shadow my-4">
-        This is some text that we have styled
-      </p>
-    </main>
-  );
+  return <main className="p-8 container mx-auto"></main>;
 }
